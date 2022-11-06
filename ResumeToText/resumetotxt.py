595a3c605e4fdfd87fde60c4ff1b4e88b0f6a916
Jesse Musa <jessemusa2@gmail.com>
2022-11-06 15:24:54 -0600

    resume to txt changes

--- ResumeToText/resumetotxt.py
+++ ResumeToText/resumetotxt.py
@@ -1,10 +1,14 @@
 # pip install pdfminer.six
 from pdfminer.high_level import extract_text
- 
+import re
+
+
 def extract_text_from_pdf(pdf_path):
     textpdf = extract_text(pdf_path)
 
     return textpdf
+
+
 
 
 if __name__ == '__main__':
